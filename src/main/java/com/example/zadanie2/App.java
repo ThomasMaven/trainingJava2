25ac8a9f08fa395edfb69f4ce649095bcfaf24bd
PGS-SOFT\ttomaka <ttomaka@pgs-software.com>
2017-03-17 13:03:00 +0100

    Create bean lifecycle examples

--- src/main/java/com/example/zadanie2/App.java
+++ src/main/java/com/example/zadanie2/App.java
@@ -24,5 +24,6 @@ public class App {
         for (String movieTitle: movieList){
             System.out.println(movieTitle);
         }
+
     }
 }
--- src/main/java/com/example/zadanie2/repo/HelloWorld.java
+++ src/main/java/com/example/zadanie2/repo/HelloWorld.java
@@ -1,15 +1,20 @@
 package com.example.zadanie2.repo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 /**
  * Created by ttomaka on 16.03.2017.
  */
 
-public class HelloWorld {
+public class HelloWorld implements InitializingBean, DisposableBean, ApplicationContextAware, BeanNameAware {
 
     private String name;
 
@@ -20,4 +25,39 @@ public class HelloWorld {
     public void printHello() {
         System.out.println("Spring : Hello ! " + name);
     }
+
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Run afterPropertiesSet method");
+    }
+    public void initHelloWorld() {
+        System.out.println("Run initHelloWorld method");
+    }
+
+    public void destroy() {
+        System.out.println("Run destroy method");
+    }
+    public void destroyHelloWorld() {
+        System.out.println("Run destroyHelloWorld method");
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Run setApplicationContext method");
+
+    }
+
+    public void setBeanName(String s) {
+        System.out.println("Run setApplicationContext method Bean name is " + s);
+
+    }
+
+    @PostConstruct
+    public void postConstructHelloWorld() {
+        System.out.println("Run postConstructHelloWorld method");
+    }
+
+    @PreDestroy
+    public void preDestroyHelloWorld() {
+        System.out.println("Run preDestroyHelloWorld method");
+    }
+
 }
